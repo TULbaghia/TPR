47a6b9a4861a78786ab9a4388047cb3c0c7ae330
michal-maksajda <treatclaire@gmail.com>
2020-10-21 13:26:21 +0200

    Katalog unit tests class added

--- Zadanie1/Zadanie1Tests/KatalogTests.cs
+++ Zadanie1/Zadanie1Tests/KatalogTests.cs
@@ -1,10 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zadanie1;
 
 namespace Zadanie1Tests
 {
-    class KatalogTests
+    [TestClass]
+    public class KatalogTests
     {
+        [TestMethod]
+        public void KatalogConstructorTest()
+        {
+            Katalog katalog = new Katalog(1, "TestowaNazwa", "TestowyProducent");
+            Assert.AreEqual(1, katalog.Id);
+            Assert.AreEqual("TestowaNazwa", katalog.Nazwa);
+            Assert.AreEqual("TestowyProducent", katalog.Producent);
+
+        }
+
+        [TestMethod]
+        public void KatalogSetTest()
+        {
+            Katalog katalog = new Katalog(1, "TestowaNazwa", "TestowyProducent");
+            katalog.Id = 2;
+            katalog.Producent = "InnyProducent";
+            katalog.Nazwa = "InnaNazwa";
+            Assert.AreEqual(2, katalog.Id);
+            Assert.AreEqual("InnyProducent", katalog.Producent);
+            Assert.AreEqual("InnaNazwa", katalog.Nazwa);
+        }
     }
 }
