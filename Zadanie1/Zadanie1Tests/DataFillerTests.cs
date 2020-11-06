1871708291d2f7eca7f0d7fd24af55726dd489cc
michal-maksajda <treatclaire@gmail.com>
2020-11-06 10:11:30 +0100

    WypelnianieLosowymi class added

--- Zadanie1/Zadanie1Tests/DataFillerTests.cs
+++ Zadanie1/Zadanie1Tests/DataFillerTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using Zadanie1.Data;
 using Zadanie1.Logic;
 
@@ -11,7 +12,20 @@ namespace Zadanie1Tests
         [TestMethod]
         public void WypelnianieStalymiTest()
         {
-            DataRepository dataRepository = new DataRepository(new WypelnianieStalymi(), new DataContext());
+            WypelnianieStalymi wypelnianieStalymi = new WypelnianieStalymi();
+            DataRepository dataRepository = new DataRepository(wypelnianieStalymi, new DataContext());
+
+            Assert.AreEqual(10, dataRepository.DataContext.Klienci.Count);
+            Assert.AreEqual(10, dataRepository.DataContext.Ksiazki.Count);
+            Assert.AreEqual(10, dataRepository.DataContext.Stany.Count);
+            Assert.AreEqual(20, dataRepository.DataContext.Zdarzenia.Count);
+        }
+
+        [TestMethod]
+        public void WypelnianieLosowymiTest()
+        {
+            WypelnianieLosowymi wypelnianieLosowymi = new WypelnianieLosowymi();
+            DataRepository dataRepository = new DataRepository(wypelnianieLosowymi, new DataContext());
 
             Assert.AreEqual(10, dataRepository.DataContext.Klienci.Count);
             Assert.AreEqual(10, dataRepository.DataContext.Ksiazki.Count);
---
+++ Zadanie1/Zadanie1Tests/WypelnianieLosowymi.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using Zadanie1.Data;
+
+namespace Zadanie1Tests
+{
+    public class WypelnianieLosowymi : IDataFiller
+    {
+        Random rand = new Random();
+
+        public void Fill(DataContext context)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                context.Klienci.Add(new Klient(RandString(rand.Next(5, 10)), RandString(rand.Next(5, 10))));
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                context.Ksiazki.Add(i, new Ksiazka(RandString(rand.Next(5, 10)), RandString(rand.Next(5, 10))));
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                context.Stany.Add(new Stan(context.Ksiazki[i], RandString(rand.Next(5, 10)),
+                    new DateTime(rand.Next(2017, 2020), rand.Next(1, 12), rand.Next(1, 27), rand.Next(8, 18), rand.Next(1, 55), rand.Next(1, 55))));
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                context.Zdarzenia.Add(new Wypozyczenie(context.Klienci[i], context.Stany[i],
+                    new DateTime(rand.Next(2017, 2020), rand.Next(1, 12), rand.Next(1, 27), rand.Next(8, 18), rand.Next(1, 55), rand.Next(1, 55))));
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                context.Zdarzenia.Add(new Zwrot(context.Klienci[i], context.Stany[i],
+                    new DateTime(rand.Next(2017, 2020), rand.Next(1, 12), rand.Next(1, 27), rand.Next(8, 18), rand.Next(1, 55), rand.Next(1, 55))));
+            }
+
+        }
+        private string RandString(int length)
+        {
+            string availableChars = "abcdefghijklmnopqrstuvwxyz";
+            StringBuilder randString = new StringBuilder();
+            Random rand = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                randString.Append(availableChars[rand.Next(availableChars.Length)]);
+            }
+
+            return randString.ToString();
+        }
+    }
+}
