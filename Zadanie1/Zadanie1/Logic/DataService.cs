dfc8a5290660ca1966e8a60e89a410687c7d8a96
michal-maksajda <treatclaire@gmail.com>
2020-11-06 23:38:40 +0100

    DataService initial version added

--- Zadanie1/Zadanie1/Logic/DataService.cs
+++ Zadanie1/Zadanie1/Logic/DataService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using Zadanie1.Data;
 
@@ -14,9 +15,136 @@ namespace Zadanie1.Logic
 
         public IDataRepository IData { get; set; }
 
-        void foo()
+        // -=-=-=-=-
+
+        public void AddKlient(Klient klient)
         {
-            IData.AddKlient(new Klient("1", "2"));
+            IData.AddKlient(klient);
         }
+        public Klient GetKlient(int id)
+        {
+            return IData.GetKlient(id);
+        }
+        public IEnumerable<Klient> GetAllKlient()
+        {
+            return IData.GetAllKlient();
+        }
+        public void UpdateKlient()
+        {
+            //
+        }
+        public void DeleteKlient(Klient klient)
+        {
+            IData.DeleteKlient(klient);
+        }
+
+        // -=-=-=-=-
+
+        public void AddKsiazka(Ksiazka ksiazka)
+        {
+            IData.AddKsiazka(ksiazka);
+        }
+        public Ksiazka GetKsiazka(int id)
+        {
+            return IData.GetKsiazka(id);
+        }
+        public IEnumerable<Ksiazka> GetAllKsiazka()
+        {
+            return IData.GetAllKsiazka();
+        }
+        public void UpdateKsiazka()
+        {
+            //
+        }
+        public void DeleteKsiazka(Ksiazka ksiazka)
+        {
+            IData.DeleteKsiazka(ksiazka);
+        }
+
+        // -=-=-=-=-
+
+        public void AddStan(Stan stan)
+        {
+            IData.AddStan(stan);
+        }
+        public Stan GetStan(int id)
+        {
+            return IData.GetStan(id);
+        }
+        public IEnumerable<Stan> GetAllStan()
+        {
+            return IData.GetAllStan();
+        }
+        public void UpdateStan()
+        {
+            //
+        }
+        public void DeleteStan(Stan stan)
+        {
+            IData.DeleteStan(stan);
+        }
+
+        // -=-=-=-=-
+
+        public void AddZdarzenie(Zdarzenie zdarzenie)
+        {
+            IData.AddZdarzenie(zdarzenie);
+        }
+        public Zdarzenie GetZdarzenie(int id)
+        {
+            return IData.GetZdarzenie(id);
+        }
+        public IEnumerable<Zdarzenie> GetAllZdarzenie()
+        {
+            return IData.GetAllZdarzenie();
+        }
+        public void UpdateZdarzenie()
+        {
+            //
+        }
+        public void DeleteZdarzenie(Zdarzenie zdarzenie)
+        {
+            IData.DeleteZdarzenie(zdarzenie);
+        }
+
+        // -=-=-=-=-
+
+        public IEnumerable<Zdarzenie> GetAllZdarzeniaDlaKsiazki(Ksiazka ksiazka)
+        {
+            List<Zdarzenie> result = new List<Zdarzenie>();
+            foreach (Zdarzenie zdarzenie in IData.GetAllZdarzenie())
+            {
+                if (zdarzenie.Stan.Ksiazka.Equals(ksiazka))
+                    result.Add(zdarzenie);
+            }
+            return result;
+        }
+
+        public IEnumerable<Zdarzenie> GetAllZdarzeniaDlaKlienta(Klient klient)
+        {
+            List<Zdarzenie> result = new List<Zdarzenie>();
+            foreach (Zdarzenie zdarzenie in IData.GetAllZdarzenie())
+            {
+                if (zdarzenie.Klient.Equals(klient))
+                    result.Add(zdarzenie);
+            }
+            return result;
+        }
+
+
+        public IEnumerable<Zdarzenie> GetAllZdarzeniaPomiedzyDatami(DateTime startTime, DateTime endTime)
+        {
+            List<Zdarzenie> zdarzenia = new List<Zdarzenie>();
+
+            foreach (Zdarzenie zdarzenie in IData.GetAllZdarzenie())
+            {
+                if (zdarzenie.Data.CompareTo(startTime) >= 0 && zdarzenie.Data.CompareTo(endTime) >= 0)
+                {
+                    zdarzenia.Add(zdarzenie);
+                }
+            }
+            return zdarzenia;
+        }
+
     }
 }
