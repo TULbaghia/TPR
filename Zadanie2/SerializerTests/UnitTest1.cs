486d73264e93b5f51635c09e96c66ecae7f0183e
Pawel Guzek <224304@edu.p.lodz.pl>
2020-11-17 13:34:48 +0100

    Exercise 2 - projects setup

---
+++ Zadanie2/SerializerTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Serializer;
+
+namespace SerializerTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
