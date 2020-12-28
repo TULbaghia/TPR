a166ccb36dcdd46a72aa9993ea7edf2313ef747e
michal-maksajda <treatclaire@gmail.com>
2020-12-28 13:48:02 +0100

    Changes: - AdventureWorks imported; - LINQ to SQL added; - tests structure added;

---
+++ Zadanie3/DatabaseTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DatabaseTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
